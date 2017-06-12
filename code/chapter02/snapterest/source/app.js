95fb10a7b1faadfd8ee7da53636326d68104e89e
hyunsoo kim <hyuntaeng@gmail.com>
2017-06-12 17:07:24 +0900

    책의 중간중간 코드 추가.

--- code/chapter02/snapterest/source/app.js
+++ code/chapter02/snapterest/source/app.js
@@ -1,6 +1,79 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+
+/*
+var reactElement = React.createElement('h1');
+ReactDOM.render(reactElement, document.getElementById('react-application'));
+*/
+
+/*
+var reactElement = React.createElement('h1', {className:'header'});
+ReactDOM.render(reactElement, document.getElementById('react-application'));
+*/
+
+/*
+var reactElement = React.createElement('h1', {className:'header'}, 'This is React');
+ReactDOM.render(reactElement, document.getElementById('react-application'));
+*/
+
+/*
+var h1 = React.createElement('h1', {className:'header', key:'header'}, 'This is React');
+var p = React.createElement('p', {className:'content', key:'content'}, "And that's how it works.");
+
+var reactFragment = [h1, p];
+var section = React.createElement('section', {className:'container'}, reactFragment);
+ReactDOM.render(section, document.getElementById('react-application'));
+*/
+
+
+/*
+var listItemElement1 = React.createElement('li', {className:'item-1', key:'item-1'}, 'Itme 1');
+var listItemElement2 = React.createElement('li', {className:'item-2', key:'item-2'}, 'Itme 2');
+var listItemElement3 = React.createElement('li', {className:'item-3', key:'item-3'}, 'Itme 3');
+
+var reactFragment = [listItemElement1, listItemElement2, listItemElement3];
+var section = React.createElement('ul', {className:'list-ofitmes'}, reactFragment);
+ReactDOM.render(section, document.getElementById('react-application'));
+*/
+
+/*
+var createListItemElement = React.createFactory('li');
+
+var listItemElement1 = createListItemElement({className:'item-1', key:'item-1'}, 'Itme 1');
+var listItemElement2 = createListItemElement({className:'item-2', key:'item-2'}, 'Itme 2');
+var listItemElement3 = createListItemElement({className:'item-3', key:'item-3'}, 'Itme 3');
+
+var reactFragment = [listItemElement1, listItemElement2, listItemElement3];
+var listOfItems = React.createElement('ul', {className:'list-ofitmes'}, reactFragment);
+ReactDOM.render(listOfItems, document.getElementById('react-application'));
+*/
+
+/*
+var listItemElement1 = React.DOM.li({className:'item-1', key:'item-1'}, 'Itme 1');
+var listItemElement2 = React.DOM.li({className:'item-2', key:'item-2'}, 'Itme 2');
+var listItemElement3 = React.DOM.li({className:'item-3', key:'item-3'}, 'Itme 3');
+
+var reactFragment = [listItemElement1, listItemElement2, listItemElement3];
+var listOfItems = React.createElement('ul', {className:'list-ofitmes'}, reactFragment);
+ReactDOM.render(listOfItems, document.getElementById('react-application'));
+*/
+
+/*
+var listItemElement1 = React.DOM.li({className:'item-1', key:'item-1'}, 'Itme 1');
+var listItemElement2 = React.DOM.li({className:'item-2', key:'item-2'}, 'Itme 2');
+var listItemElement3 = React.DOM.li({className:'item-3', key:'item-3'}, 'Itme 3');
+
+var reactFragment = [listItemElement1, listItemElement2, listItemElement3];
+var listOfItems = React.createElement('ul', {className:'list-ofitmes'}, reactFragment);
+ReactDOM.render(listOfItems, document.getElementById('react-application'));
+
+var ReactDOMServer = require('react-dom/server');
+console.log(ReactDOMServer.renderToString(listOfItems));
+console.log(ReactDOMServer.renderToStaticMarkup(listOfItems));
+*/
+
+
 var listOfItems = <ul className="list-of-items">
                    <li className="item-1">Item 1</li>
                    <li className="item-2">Item 2</li>
@@ -8,3 +81,4 @@ var listOfItems = <ul className="list-of-items">
                   </ul>;
 
 ReactDOM.render(listOfItems, document.getElementById('react-application'));
+
